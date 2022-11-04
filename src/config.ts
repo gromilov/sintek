64ebb8e874b6a16b1cab24a1fab476c3d5a33f76
gromilov <max.gromilov@yandex.ru>
2022-11-05 01:32:32 +0300

    start

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Sintek - Ваш проект достигнет высот';
+export const SITE_DESCRIPTION = 'Разработка и сопровождение digital проектов';
